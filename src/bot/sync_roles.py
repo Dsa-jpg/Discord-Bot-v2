607f3c22d72b820ecb255f4bc6263b0b33c21ce5
Filip Nachtman <65063262+Dsa-jpg@users.noreply.github.com>
2025-09-13 22:10:41 +0200

    Update sync_roles.py

--- src/bot/sync_roles.py
+++ src/bot/sync_roles.py
@@ -5,14 +5,13 @@ import discord
 import yaml
 from dotenv import load_dotenv
 
+# Přidání root projektu do sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from cogs.reaction_roles import ReactionRoles
 
-
 load_dotenv()
 TOKEN = os.getenv("DISCORD_KEY")
-
 
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), "../config/config.yaml")
 with open(CONFIG_PATH, "r", encoding="utf-8") as f:
@@ -20,18 +19,14 @@ with open(CONFIG_PATH, "r", encoding="utf-8") as f:
 
 intents = discord.Intents.all()
 bot = discord.Client(intents=intents)
-bot.config = config 
+bot.config = config
 
 @bot.event
 async def on_ready():
     print(f"Přihlášen jako {bot.user}")
-
     cog = ReactionRoles(bot)
-
-   
-    await cog.sync_all(add_only=True)  
-    print("SYNC dokončen, bot se zavře.")
+    await cog.sync_all(add_only=True)  # jen přidání chybějících rolí
+    print("SYNC dokončen, bot se zavírá")
     await bot.close()
 
-if __name__ == "__main__":
-    asyncio.run(bot.start(TOKEN))
+asyncio.run(bot.start(TOKEN))
